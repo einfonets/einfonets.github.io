b593ad9eaa2158452ef9883b45579264b9f54fa1
einfonets <einfonets@gmail.com>
2023-01-15 22:34:06 +0530

    baseurl changes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 const path = require("path");
-const { default: baseUrl } = require("./utils/baseUrl");
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/*",
-        destination: `${baseUrl}/api/*`,
-      },
-    ];
-  },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: "/api/*",
+  //         destination: `https://einfonets.com/api/*`,
+  //       },
+  //     ];
+  //   },
 };
