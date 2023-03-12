538292feec5d84123dc4b18b0e1ff302c1a55671
einfonets <einfonets@gmail.com>
2023-03-12 16:54:33 +0530

    title changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,54 +1,76 @@
-import '../styles/bootstrap.min.css'
-import '../styles/animate.min.css'
-import '../styles/boxicons.min.css'
-import 'react-accessible-accordion/dist/fancy-example.css'
-import '../node_modules/react-modal-video/css/modal-video.min.css'
-import '../styles/style.css'
-import '../styles/responsive.css'
+import "../styles/bootstrap.min.css";
+import "../styles/animate.min.css";
+import "../styles/boxicons.min.css";
+import "react-accessible-accordion/dist/fancy-example.css";
+import "../node_modules/react-modal-video/css/modal-video.min.css";
+import "../styles/style.css";
+import "../styles/responsive.css";
 
-import React from 'react'
-import App from 'next/app'
-import Head from "next/head"
-import Loader from '../components/Shared/Loader'
-import GoTop from '../components/Shared/GoTop'
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import Loader from "../components/Shared/Loader";
+import GoTop from "../components/Shared/GoTop";
 
 export default class MyApp extends App {
-
-    // Preloader
-    state = {
-        loading: true
-    };
-    componentDidMount() {
-        this.timerHandle = setTimeout(() => this.setState({ loading: false }), 2000);
+  // Preloader
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    this.timerHandle = setTimeout(
+      () => this.setState({ loading: false }),
+      2000
+    );
+  }
+  componentWillUnmount() {
+    if (this.timerHandle) {
+      clearTimeout(this.timerHandle);
+      this.timerHandle = 0;
     }
-    componentWillUnmount() {
-        if (this.timerHandle) {
-            clearTimeout(this.timerHandle);
-            this.timerHandle = 0;
-        }
-    }
+  }
 
-    render() {
-        const { Component, pageProps } = this.props
-        return (
-            <>
-                <Head>
-                    <title>Product Engineering Services | Digital Transformation Cloud Solutions</title>
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <meta name="description" content="A top software development company in Canada & India delivering web and mobile app solutions to global startups, businesses & enterprises." />
-                    <meta name="og:title" property="og:title" content="Product Engineering Services | Digital Transformation Cloud Solutions"></meta>
-                    <meta name="twitter:card" content="Product Engineering Services | Digital Transformation Cloud Solutions"></meta>
-                    <link rel="canonical" href="http://www.einfonets.com/"></link>
-                </Head>
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <title>einfonets | Leading IT Solutions Provider</title>
+          {/* <title>Product Engineering Services | Digital Transformation Cloud Solutions</title> */}
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
 
-                <Component {...pageProps} />
+          {/* <meta
+            name="description"
+            content="A top software development company in Canada & India delivering web and mobile app solutions to global startups, businesses & enterprises."
+          /> */}
 
-                {/* Preloader */}
-                <Loader loading={this.state.loading} />
+          <meta
+            name="description"
+            content="Secure IT Solutions for a more secure environment. We are a Digital and IT based organization that provides a wide variety of services, from site creation to technology advice. "
+          />
+          <metas
+            name="og:title"
+            property="og:title"
+            content="einfonets | Leading IT Solutions Provider"
+          ></metas>
+          <meta
+            name="twitter:card"
+            content="einfonets | Leading IT Solutions Provider"
+          ></meta>
+          <link rel="canonical" href="http://www.einfonets.com/"></link>
+        </Head>
 
-                {/* Go Top Button */}
-                <GoTop scrollStepInPx="100" delayInMs="10.50" />
-            </>
-        );
-    }
+        <Component {...pageProps} />
+
+        {/* Preloader */}
+        <Loader loading={this.state.loading} />
+
+        {/* Go Top Button */}
+        <GoTop scrollStepInPx="100" delayInMs="10.50" />
+      </>
+    );
+  }
 }
