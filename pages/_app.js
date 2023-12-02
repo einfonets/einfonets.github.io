b93d27244083805b35117d9e8a11d9a2e0f897f9
einfonets <einfonets@gmail.com>
2023-12-02 22:09:47 +0530

    added gtm configs

--- pages/_app.js
+++ pages/_app.js
@@ -35,6 +35,17 @@ export default class MyApp extends App {
     return (
       <>
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WCF3SDZ8');
+      `,
+            }}
+          />
           <title>
             Software Professional | Einfonets | Software Company | Leading IT
             Solutions Provider
@@ -82,6 +93,15 @@ export default class MyApp extends App {
           <meta property="og:type" content="website" />
           <link rel="canonical" href="https://www.einfonets.com"></link>
         </Head>
+
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCF3SDZ8"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `,
+          }}
+        />
 
         <Component {...pageProps} />
 
